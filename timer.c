750b437ea32000f2d0d91b2bb2d8e4aa68fe9e5e
funkyboris <funkyboris@gmail.com>
2013-02-16 20:01:53 +0100

    Refactored main.c and removed unused funtionality
    Cleaned up main method in main.c and factored out
    some of the functionality to functions to
    improve readability of the code.

--- timer.c
+++ timer.c
@@ -111,7 +111,9 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(TIMER2_COMP_vect)
 {
-	if (++minuteCounter == 10)
-		minuteCounter = 0;
+   // deepSleep() has taken over this functionality.
+
+	//if (++minuteCounter == 10)
+	//	minuteCounter = 0;
 	//MORSEPORT ^= 0x10;
 }
